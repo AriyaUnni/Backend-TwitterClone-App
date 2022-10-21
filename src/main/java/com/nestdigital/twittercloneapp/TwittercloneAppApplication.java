b0ed016aab89235afe802f61daface5f988cf2c5
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-21 12:13:48 +0530

    first commit

---
+++ src/main/java/com/nestdigital/twittercloneapp/TwittercloneAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.twittercloneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittercloneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittercloneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/twittercloneapp/TwittercloneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.twittercloneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittercloneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
